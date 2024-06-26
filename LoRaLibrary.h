22c45864cd7c8d86e2b3c4ce3618e76d4c2fa31a
Enzo Moura <mouraenzo21@gmail.com>
2024-06-26 07:38:42 -0300

    initial commit.

--- LoRaLibrary.h
+++ LoRaLibrary.h
@@ -11,21 +11,6 @@
 
 extern RH_RF95 rf95;
 
-struct Message {
-  unsigned int message_type: 1;
-  unsigned int id: 15;
-  unsigned long latitude: 24;
-  unsigned long longitude: 24;
-  unsigned int group_flag: 1;
-  unsigned int record_time: 16;
-  unsigned int max_records: 11;
-  unsigned int hop_count: 4;
-  unsigned int channel: 2;
-  unsigned int location_time: 16;
-  unsigned int help_flag: 2;
-  unsigned int battery: 4;
-};
-
 void setupLoRa();
 void sendMessage(uint8_t* data, uint8_t length);
 void receiveMessage();
