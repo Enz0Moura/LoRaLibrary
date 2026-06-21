5793d93a5a63ebc10583a78ca37984f52d23f698
Enzo Moura <mouraenzo21@gmail.com>
2026-06-21 02:01:41 -0300

    feat: add cross-platform serial support.

---
+++ libs/locino_host/src/SerialPortPosix.cpp
@@ -0,0 +1,83 @@
+#include "LocINO/SerialPort.hpp"
+
+#ifndef _WIN32
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+
+namespace LocINO {
+
+SerialPort::SerialPort(const std::string& device, int baudRate) {
+    _fd = open(device.c_str(), O_RDWR | O_NOCTTY | O_SYNC);
+
+    if (_fd >= 0) {
+        configure(baudRate);
+    }
+}
+
+SerialPort::~SerialPort() {
+    if (_fd >= 0) {
+        close(_fd);
+    }
+}
+
+bool SerialPort::configure(int baudRate) {
+    termios tty{};
+
+    if (tcgetattr(_fd, &tty) != 0) {
+        return false;
+    }
+
+    speed_t speed = B9600;
+
+    if (baudRate == 115200) {
+        speed = B115200;
+    }
+
+    cfsetospeed(&tty, speed);
+    cfsetispeed(&tty, speed);
+
+    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;
+    tty.c_cflag |= CLOCAL | CREAD;
+    tty.c_cflag &= ~PARENB;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CRTSCTS;
+
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+    tty.c_lflag = 0;
+    tty.c_oflag = 0;
+
+    tty.c_cc[VMIN] = 1;
+    tty.c_cc[VTIME] = 10;
+
+    return tcsetattr(_fd, TCSANOW, &tty) == 0;
+}
+
+bool SerialPort::writeBytes(const uint8_t* data, size_t length) {
+    return _fd >= 0 &&
+           write(_fd, data, length) == static_cast<ssize_t>(length);
+}
+
+bool SerialPort::readByte(uint8_t& byte) {
+    return _fd >= 0 && read(_fd, &byte, 1) == 1;
+}
+
+bool SerialPort::readBytes(uint8_t* data, size_t length) {
+    size_t total = 0;
+
+    while (total < length) {
+        ssize_t n = read(_fd, data + total, length - total);
+
+        if (n <= 0) {
+            return false;
+        }
+
+        total += static_cast<size_t>(n);
+    }
+
+    return true;
+}
+
+}
+
+#endif
---
+++ libs/locino_host/src/SerialPortWindows.hpp
@@ -0,0 +1,149 @@
+#include "LocINO/SerialPort.hpp"
+
+#ifdef _WIN32
+
+namespace LocINO {
+
+static DWORD toWindowsBaudRate(int baudRate) {
+    switch (baudRate) {
+        case 9600:
+            return CBR_9600;
+        case 19200:
+            return CBR_19200;
+        case 38400:
+            return CBR_38400;
+        case 57600:
+            return CBR_57600;
+        case 115200:
+            return CBR_115200;
+        default:
+            return CBR_9600;
+    }
+}
+
+static std::string normalizeDeviceName(const std::string& device) {
+    if (device.rfind("\\\\.\\", 0) == 0) {
+        return device;
+    }
+
+    if (device.rfind("COM", 0) == 0) {
+        return "\\\\.\\" + device;
+    }
+
+    return device;
+}
+
+SerialPort::SerialPort(const std::string& device, int baudRate) {
+    std::string normalizedDevice = normalizeDeviceName(device);
+
+    _handle = CreateFileA(
+        normalizedDevice.c_str(),
+        GENERIC_READ | GENERIC_WRITE,
+        0,
+        nullptr,
+        OPEN_EXISTING,
+        0,
+        nullptr
+    );
+
+    if (_handle != INVALID_HANDLE_VALUE) {
+        configure(baudRate);
+    }
+}
+
+SerialPort::~SerialPort() {
+    if (_handle != INVALID_HANDLE_VALUE) {
+        CloseHandle(_handle);
+    }
+}
+
+bool SerialPort::configure(int baudRate) {
+    DCB dcb{};
+    dcb.DCBlength = sizeof(dcb);
+
+    if (!GetCommState(_handle, &dcb)) {
+        return false;
+    }
+
+    dcb.BaudRate = toWindowsBaudRate(baudRate);
+    dcb.ByteSize = 8;
+    dcb.Parity = NOPARITY;
+    dcb.StopBits = ONESTOPBIT;
+
+    dcb.fBinary = TRUE;
+    dcb.fDtrControl = DTR_CONTROL_ENABLE;
+    dcb.fRtsControl = RTS_CONTROL_ENABLE;
+
+    if (!SetCommState(_handle, &dcb)) {
+        return false;
+    }
+
+    COMMTIMEOUTS timeouts{};
+    timeouts.ReadIntervalTimeout = 50;
+    timeouts.ReadTotalTimeoutConstant = 1000;
+    timeouts.ReadTotalTimeoutMultiplier = 10;
+    timeouts.WriteTotalTimeoutConstant = 1000;
+    timeouts.WriteTotalTimeoutMultiplier = 10;
+
+    if (!SetCommTimeouts(_handle, &timeouts)) {
+        return false;
+    }
+
+    PurgeComm(_handle, PURGE_RXCLEAR | PURGE_TXCLEAR);
+
+    return true;
+}
+
+bool SerialPort::writeBytes(const uint8_t* data, size_t length) {
+    if (_handle == INVALID_HANDLE_VALUE) {
+        return false;
+    }
+
+    DWORD written = 0;
+
+    BOOL ok = WriteFile(
+        _handle,
+        data,
+        static_cast<DWORD>(length),
+        &written,
+        nullptr
+    );
+
+    return ok && written == length;
+}
+
+bool SerialPort::readByte(uint8_t& byte) {
+    return readBytes(&byte, 1);
+}
+
+bool SerialPort::readBytes(uint8_t* data, size_t length) {
+    if (_handle == INVALID_HANDLE_VALUE) {
+        return false;
+    }
+
+    size_t total = 0;
+
+    while (total < length) {
+        DWORD bytesRead = 0;
+
+        BOOL ok = ReadFile(
+            _handle,
+            data + total,
+            static_cast<DWORD>(length - total),
+            &bytesRead,
+            nullptr
+        );
+
+        if (!ok || bytesRead == 0) {
+            return false;
+        }
+
+        total += bytesRead;
+    }
+
+    return true;
+}
+
+}
+
+#endif
